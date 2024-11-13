33a6a9596963b5045ac671f6808af029e713600e
FranciscoFernandez <fransmandes@gmail.com>
2024-11-13 01:48:15 -0300

    responsive lindo

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,17 @@
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+
+
+abrir.addEventListener("click", () => {
+  nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+  nav.classList.remove("visible");
+})
+
 const carrusel = [
 
 ];
@@ -66,3 +80,5 @@ $(document).ready(function () {
     }
   });
 });
+
+
